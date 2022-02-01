f04a2e040b1486d708beee33a5afe296b0a4b477
NoahBarkoff <nbarkoff@colgate.edu>
2022-02-01 15:58:17 -0500

    Finished question 2 of activity 1

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,28 @@
+# GEOG311 Activity 1 Script
+# NB 2/1/2022
+
 print("Hello World")
+6^6
+# name my number
+a <- 6234
+
+# multiply a by 5 
+a*5
+
+#rename my number
+a <- 5
+
+# set b to equal a vector with a as a variable
+b <- c(a,10,15,20,25)
+
+# divide each variable in b by 2
+b/2
+
+# divide each variable in b by a, which is equal to 5
+b/a
+
+# divide only the second variable by a
+b[2]/a
+
+# multiply each variable in b by itself
+b*b
