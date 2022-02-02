1f120f7442d03b461e74759d53ab264f8f2bc9d2
NoahBarkoff <nbarkoff@colgate.edu>
2022-02-02 11:56:49 -0500

    Worked on Activity 2 during class 2/2/2022

--- Activity2.R
+++ Activity2.R
@@ -1,6 +1,9 @@
 # GEOG311 Activity 2 Script
 # NB 1/31/2022
 
+# Set working directory to my noaa data folder
+setwd("Z:/students/nbarkoff/Data/noaa_weather")
+
 # Make a vector of tree heights in meters
 heights <- c(30,41,20,22)
 
@@ -16,3 +19,16 @@ help (matrix)
 # Set up a matrix with 2 columns and fill in by rows
 # First argument is the vector of numbers to fill in the matrix
 Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
+
+# Set up a matrix that fills in by columns
+# First argument is the vector of numbers to fill in the matrix
+Mat.bycol<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=FALSE)
+
+# Look at the number in the first row and second column
+Mat.bycol[1,2]
+
+# Look at all the numbers in the second column
+Mat.bycol[,2]
+
+# Read weather data
+datw <- read.csv("2011124.csv")
