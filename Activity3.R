07367e9efa95118cd3fd5bd96e50fec3af4cd596
NoahBarkoff <nbarkoff@colgate.edu>
2022-03-02 11:15:05 -0500

    Roughly finished all the activity 3 questions

--- Activity3.R
+++ Activity3.R
@@ -96,8 +96,10 @@ points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
 # Just creating an object with the same data already in the DatW dataset
 assert(length(lightscale)==length(datW$wind.speed),
        "error: unequal lengths")
+
 # ---------------
 
+# Question 6
 
 # Filter out the storms where there is some of rain and lightning
 # As well as storms where there is lots of rain
@@ -105,7 +107,6 @@ assert(length(lightscale)==length(datW$wind.speed),
 datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                           ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
 
-# Question 6
 datW$wind.speedQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                           ifelse(datW$precipitation > 5, NA, datW$wind.speed))
 
@@ -114,7 +115,7 @@ assert(length(which(is.na(datW$wind.speedQ2)))==length(which(is.na(datW$wind.spe
                           "error: unequal number of NA oberservations")
 
 plot(datW$DD , datW$wind.speedQ2, xlab = "Day of Year", ylab = "Wind Speed",
-     type="n")
+     type="l")
 
 points(datW$wind.speedQ2, col= "blue", pch=15)
 
@@ -122,8 +123,15 @@ points(datW$wind.speedQ2, col= "blue", pch=15)
 #---------------
 
 # Question 7
-plot(datW$DD , datW$air.tempQ1, xlab = "Day of Year", ylab = "Soil & Air Temperature",
-     type="n", xlim=c(185,200))
+
+plot(datW$DD , datW$air.tempQ1, xlab = "Day of Year", ylab = "Air Temperature",
+     type="l", xlim=c(180,193))
+plot(datW$DD , datW$soil.temp, xlab = "Day of Year", ylab = "Soil Temperature",
+     type="l", xlim=c(180,193))
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation",
+     type="l", xlim=c(180,193))
+plot(datW$DD , datW$soil.moisture, xlab = "Day of Year", ylab = "Soil Moisture",
+     type="l", xlim=c(180,193))
 
 points(datW$soil.temp, col= "blue", pch=15)
 points(datW$air.tempQ1, col= "red", pch=15)
@@ -143,48 +151,54 @@ soil.tempQ1 <- na.omit(datW$soil.temp)
 #-----------
 
 # Question 8
-# Create three vectors 
-name <- c("al", "bea", "carol")
-age <- c(6, 7, 4)
-hair <- c("brown", "green", "blond")
 
-# Create data frame 
-#children <- data.frame(name, age, hair)
-#children
-#name age  hair
-#1    al   6 brown
-#2   bea   7 green
-#3 carol   4 blond
-
-#ave (x, FUN=mean, )
-#help(NA)
+# Create requested table
 
 Ave.AirTemp <- signif(mean (datW$air.temperature, na.rm = TRUE),3)
 Ave.WindSpeed <- signif(mean (datW$wind.speedQ2, na.rm =TRUE),3)
 Ave.SoilMoisture <- signif(mean (datW$soil.moisture, na.rm =TRUE),8)
 Ave.Precipitation <- signif(mean (datW$precipitation, na.rm =TRUE),4)
-length
-Requested.Table <- data.frame (Ave.AirTemp,Ave.WindSpeed, Ave.SoilMoisture, Ave.Precipitation )
+Ave.SoilTemp <-signif(mean (datW$soil.temp, na.rm =TRUE),3)
+
+Requested.Table <- data.frame (Ave.AirTemp,Ave.WindSpeed, Ave.SoilMoisture, Ave.Precipitation, Ave.SoilTemp )
+
+# Calculate number of observations in each object
+
+length(datW$air.temperature[!is.na(datW$air.temperature)])
+length(datW$wind.speedQ2[!is.na(datW$wind.speedQ2)])
+length(datW$soil.moisture[!is.na(datW$soil.moisture)])
+length(datW$precipitation[!is.na(datW$precipitation)])
+length(datW$soil.temp[!is.na(datW$soil.temp)])
+
+# Calculate time period for each observation
+
+length(datW$air.temperature[!is.na(datW$air.temperature)])*30/60/24
+length(datW$wind.speedQ2[!is.na(datW$wind.speedQ2)])*30/60/24
+length(datW$soil.moisture[!is.na(datW$soil.moisture)])*30/60/24
+length(datW$precipitation[!is.na(datW$precipitation)])*30/60/24
+length(datW$soil.temp[!is.na(datW$soil.temp)])*30/60/24
+
 
 # ------------------
 # Question 9
 par(mfrow=c(2,2))
 
 plot (datW$DD, datW$air.temperature, xlab = "Day of Year", 
-      ylab = "Air Temperature", type="n", main="Air Temperature in the Summer")
-points(datW$air.temperature, col="blue", pch=15)
+      ylab = "Air Temperature", type="l", main="Air Temperature in the Summer")
 
 plot (datW$DD, datW$soil.moisture, xlab = "Day of Year", 
-      ylab = "Soil Moisture", type="n", main="Soil Moisture in the Summer")
-points(datW$soil.moisture, col="blue", pch=15)
+      ylab = "Soil Moisture", type="l", main="Soil Moisture in the Summer")
 
 plot (datW$DD, datW$soil.temp, xlab = "Day of Year", 
-      ylab = "Soil Temperature", type="n", main="Soil Temperature in the Summer")
-points(datW$soil.temp, col="blue", pch=15)
+      ylab = "Soil Temperature", type="l", main="Soil Temperature in the Summer")
 
 plot (datW$DD, datW$precipitation, xlab = "Day of Year", 
-      ylab = "Precipitation", type="n", ylim=c(0,-------), main="Precipitation in the Summer")
-points(datW$precipitation > -------, col="blue", pch=15)
+      ylab = "Precipitation", type="l", main="Precipitation in the Summer")
+
+points(datW$air.temperature, col="blue", pch=15)
+points(datW$soil.moisture, col="blue", pch=15)
+points(datW$soil.temp, col="blue", pch=15)
+points(datW$precipitation, col="blue", pch=15)
 
 
 
