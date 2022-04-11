41ea877f6b12618e54773a86f60ef139c9c10e20
NoahBarkoff <nbarkoff@colgate.edu>
2022-04-11 12:09:57 -0400

    Manipulated data sets

--- FInalProject.R
+++ FInalProject.R
@@ -4,6 +4,7 @@
 
 # Library dplyr
 library("dplyr")
+library("tidyverse")
 
 # Read in data files
 
@@ -13,20 +14,26 @@ PrecipLM <- read.csv("Z:/students/nbarkoff/Data/prc_mic_basn_mon.csv",
 # USE PIVOT LONGER
 # https://stackoverflow.com/questions/22286419/move-a-column-to-first-position-in-a-data-frame
 
+# Turn columns to rows
+PrecipLM.COltoRow <- pivot_longer(PrecipLM, cols = (2:13))
 
-########### Also ????????????? ############
-PrecipLMt <- as.data.frame(t(PrecipLM))
+# Change column names
+colnames(PrecipLM.COltoRow) <- c("year","month", "precipitation(mm)")
 
+# Move columns so each data frame has same format
+datPrecipLM <- PrecipLM.COltoRow %>% select("month", "year", "precipitation(mm)")
 
-
-
-########### ?????????????????? ############
-Month.cols <- rep(c("Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec"),
-                  time = 82)
-
-PrecipLM$month <- Month.cols
 
 # Read in all Great Lakes water level data
 WaterLevels <- read.csv("Z:/students/nbarkoff/Data/GLHYD_data_metric.csv",
                         header = TRUE, skip = 12)
 
+# Only include relevant columns
+datWaterLevelsLM <- WaterLevels[,c(1,2,4)]
+
+# Change column names
+colnames(datWaterLevelsLM) <- c("year","month", "waterlevel(m)")
+
+# Include only shared years between data sets
+
+# https://stackoverflow.com/questions/12328056/how-do-i-delete-rows-in-a-data-frame
