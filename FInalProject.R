b674277fd561956593b46e0f15c71275c8b2e5e2
NoahBarkoff <nbarkoff@colgate.edu>
2022-04-19 19:23:32 -0700

    Finished first figure of final project

--- FInalProject.R
+++ FInalProject.R
@@ -5,70 +5,48 @@
 # Library dplyr
 library("dplyr")
 library("tidyverse")
-library("ggplot2")
-
-# Read in data files
 
 # Read in Lake Michigan (LM) basin precipitation data
 PrecipLM <- read.csv("Z:/students/nbarkoff/Data/prc_mic_basn_mon.csv",
                    header = TRUE, skip = 5)
-# USE PIVOT LONGER
-# https://stackoverflow.com/questions/22286419/move-a-column-to-first-position-in-a-data-frame
 
-# Turn columns to rows
+# Turn PrecipLM columns to rows
 PrecipLM.COltoRow <- pivot_longer(PrecipLM, cols = (2:13))
 
-# Change column names
+# Change PrecipLM column names
 colnames(PrecipLM.COltoRow) <- c("year","month", "precipitation")
 
-# Move columns so each data frame has same format
+# Move PrecipLM columns so each data frame has same format
 datPrecipLM <- PrecipLM.COltoRow %>% select("month", "year", "precipitation")
-
 
 # Read in all Great Lakes water level data
 WaterLevels <- read.csv("Z:/students/nbarkoff/Data/GLHYD_data_metric.csv",
                         header = TRUE, skip = 12)
 
-# Only include relevant columns
+# Only include relevant waterLevel columns
 datWaterLevelsLM <- WaterLevels[,c(1,2,4)]
 
-# Change column names
+# Change waterLevel column names
 colnames(datWaterLevelsLM) <- c("month","year", "waterlevel")
 
-# Include only shared years between data sets
-
+# Include only shared years between waterLevels and precipitation data sets
 datPrecipLMf <- datPrecipLM[-c(973:984), ]
 datWaterLevelsLMf <- datWaterLevelsLM[c(265:1236),  ]
 
-# Plot overall trend in precipitation data and water level data on same graph
-
+# Make new data frame with only the variables being plotted for a linear regression
 datLinear.Model <- data.frame (datWaterLevelsLMf$`waterlevel`, datPrecipLMf$`precipitation`)
 
 # Name datLinear.Model column names
 colnames(datLinear.Model) <- c("waterlevel", "precipitation")
 
-datLinear.Model$precipitation = as.numeric(as.character(datLinear.Model$precipitation))
-
-# https://www.statology.org/line-of-best-fit-in-r/
-
-
-#Try using base plotting
+# Plot precipitation and water level along with a linear regression
 plot(datLinear.Model$waterlevel, datLinear.Model$precipitation, pch = 16, cex = 1.3, col = "blue", xlab="Water Level (M)", 
      ylab= "Precipitation(mm)", 
-     main = "Name")
+     main = "Preciptation's influence on Water Level in Lake Michigan from 1940-2020")
+legend("topright", c("Precipitation values","Linear Regression Model"),
+       lwd=c(NA,2),
+       col=c("blue","red"),
+       pch=c(16,NA),
+       bty="n")
+abline(lm(precipitation~waterlevel, data = datLinear.Model), lwd = 2, col = "red")
 
-# Try 1
-abline (lm(waterlevel ~ precipitation, data = datLinear.Model))
-
-# Try 2
-abline(Liner.Model)
-Linear.Model <- lm(waterlevel ~ precipitation, data = datLinear.Model)
-
-
-# Try using ggplot2 - 3
-ggplot(data = datLinear.Model, mapping = aes(x = waterlevel, y = precipitation)) + 
-  geom_point() + theme_bw() + ggtitle("Name") +
-  geom_smooth(method=lm, se=FALSE)
-
-
-# https://stackoverflow.com/questions/12328056/how-do-i-delete-rows-in-a-data-frame
